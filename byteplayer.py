e4acead7f8063746c3a6116c36be4f707d9e74e6
robotanarchy <robotanarchy@bingo-ev.de>
2013-12-30 04:57:48 +0100

    code cleanup, smaller changes (such as: when hitting return in form field, open action gets performed (for JS version only currently))

--- byteplayer.py
+++ byteplayer.py
@@ -32,7 +32,7 @@ class byteplayer:
 	
 	def open(self,url):
 		# TODO: improve code, the try catch stuff prints an
-		# exception to the terminal!
+		# exception (caused by youtube-dl) to the terminal!
 		# Also to avoid nested try catch statements, create a wrapper
 		# function as soon as there are more URL grabbers
 		try:
