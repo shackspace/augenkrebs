0eca77ce438460093f4c1ae3b907696a5a55b699
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-03-03 09:05:29 +0100

    try to create ~/.config/byteplayer with mode 0700 on startup

--- history.py
+++ history.py
@@ -6,6 +6,10 @@ class history():
 	def __init__(self):
 		conf_dir = os.getenv('HOME') + '/.config/byteplayer/'
 		history_location = conf_dir + 'history.db'
+		try:
+			os.makedirs(conf_dir,mode=0o700,exist_ok=True)
+		except OSError as oe:
+			print(oe)
 		self._connection = sqlite3.connect(history_location)
 		cursor = self._connection.cursor()
 		cursor.execute(''' create table if not exists HISTORY 
