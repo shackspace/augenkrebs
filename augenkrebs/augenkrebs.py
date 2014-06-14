671007ed0a66c59c82946df3c28ceac5a4da0319
rixx <git@foyfoy.de>
2014-06-14 13:29:48 +0200

    static files can now be served from frontend/public

--- augenkrebs/augenkrebs.py
+++ augenkrebs/augenkrebs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from flask import send_from_directory
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__, static_url_path='', static_folder='frontend/public')
+
+@app.route('/', methods=['GET', 'POST'])
+def augenkrebs():
+    if request.method == 'GET':
+        return app.send_static_file('index.html')
+    elif request.method == 'POST':
+        return 'POST WORLLLLD!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
