375732586dea4b2a5c9618c5e6de73daa021e52e
rixx <git@foyfoy.de>
2014-06-14 15:31:47 +0200

    can now parse and return json, basic routes are there

--- augenkrebs/augenkrebs.py
+++ augenkrebs/augenkrebs.py
@@ -3,15 +3,26 @@
 from flask import Flask
 from flask import request
 from flask import send_from_directory
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/public')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def augenkrebs():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/open', methods=['POST'])
+def api_open():
+    bla = {"rand" : "h"}
+    keks = str(request.json['path'])
+    return json.dumps(bla)
+
+
+@app.route('/api/status', methods=['GET', 'POST'])
+def api_status():
     if request.method == 'GET':
-        return app.send_static_file('index.html')
-    elif request.method == 'POST':
-        return 'POST WORLLLLD!'
+        return 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
